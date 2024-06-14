688858efa3fceaa2510ebfea646bf0c3c58d0710
sam-pace <sl.spacce2000@gmail.com>
2024-06-13 21:44:37 -0300

    feat: added human navigation

--- index.js
+++ index.js
@@ -1,51 +1,63 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 const fs = require('fs');
-const {authenticate, sendMessage} = require('./handleWhatsApp');
+const { authenticate, sendMessage } = require('./handleWhatsApp');
 const player = require('play-sound')();
 require('dotenv').config();
 
+puppeteer.use(StealthPlugin());
+
+const randomDelay = (min, max) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+const wait = (ms) => new Promise(resolve => setTimeout(resolve, ms));
 
 (async () => {
-
   const selectors = {
-      login: {
-          email: 'input[name="Email"]',
-          password: 'input[type="password"]',
-          button: '#login-form > button'
-      },
-      serviceRow: '#dataTableServices > tbody > tr:nth-child(3)',
-      serviceButton: '#dataTableServices > tbody > tr:nth-child(3) button'
+    login: {
+      email: 'input[name="Email"]',
+      password: 'input[type="password"]',
+      button: '#login-form > button'
+    },
+    serviceRow: '#dataTableServices > tbody > tr:nth-child(3)',
+    serviceButton: '#dataTableServices > tbody > tr:nth-child(3) button'
   };
 
   const msg = {
-      login: 'Aviso: Novo login em sua conta do consulado',
-      booking: 'Olá! Encontramos uma data disponível para agendamento.'
-  }
+    login: 'Aviso: Novo login em sua conta do consulado',
+    booking: 'Aviso: Encontramos uma data disponível para agendamento!'
+  };
 
   const urls = {
-      base: 'https://prenotami.esteri.it/',
-      login: 'https://prenotami.esteri.it/Home?ReturnUrl=%2fServices',
-      services: 'https://prenotami.esteri.it/Services',
-      booking: 'https://prenotami.esteri.it/Services/Booking/2427'
+    base: 'https://prenotami.esteri.it/',
+    login: 'https://prenotami.esteri.it/Home?ReturnUrl=%2fServices',
+    services: 'https://prenotami.esteri.it/Services',
+    booking: 'https://prenotami.esteri.it/Services/Booking/2427'
   };
 
   const login = async (page) => {
-      try {
-          await page.waitForSelector(selectors.login.email);
-          await page.waitForSelector(selectors.login.password);
+    try {
+      await page.waitForSelector(selectors.login.email);
+      await page.waitForSelector(selectors.login.password);
 
-          await page.type(selectors.login.email, process.env.EMAIL);
-          await page.type(selectors.login.password, process.env.PASSWORD);
-          await page.click(selectors.login.button);
-
-          await page.waitForNavigation({ waitUntil: 'networkidle0' });
-
-          const cookies = await page.cookies();
-          fs.writeFileSync('./cookies.json', JSON.stringify(cookies, null, 2));
-      } catch (error) {
-          console.error('Error during login:', error);
-          throw error;
+      // Simulate human typing with random delays
+      for (const char of process.env.EMAIL) {
+        await page.type(selectors.login.email, char, { delay: randomDelay(30, 150) });
       }
+
+      for (const char of process.env.PASSWORD) {
+        await page.type(selectors.login.password, char, { delay: randomDelay(30, 150) });
+      }
+
+      await simulateUserInteraction(page, selectors.login.button);
+
+      await page.waitForNavigation({ waitUntil: 'networkidle0' });
+
+      const cookies = await page.cookies();
+      fs.writeFileSync('./cookies.json', JSON.stringify(cookies, null, 2));
+    } catch (error) {
+      console.error('Error during login:', error);
+      throw error;
+    }
   };
 
   const configurePage = async (page) => {
@@ -55,6 +67,14 @@ require('dotenv').config();
     await page.evaluateOnNewDocument(() => {
       Object.defineProperty(navigator, 'webdriver', {
         get: () => undefined,
+      });
+
+      Object.defineProperty(navigator, 'languages', {
+        get: () => ['en-US', 'en'],
+      });
+
+      Object.defineProperty(navigator, 'plugins', {
+        get: () => [1, 2, 3, 4, 5],
       });
 
       const originalQuery = window.navigator.permissions.query;
@@ -69,51 +89,58 @@ require('dotenv').config();
   };
 
   const playSound = () => {
-    player.play('sound/alarm-clock.mp3', function(err){
-      if (err) throw err
+    player.play('sound/alarm-clock.mp3', function(err) {
+      if (err) throw err;
     });
+  };
+
+  const simulateUserInteraction = async (page, selector) => {
+    const element = await page.$(selector);
+    const box = await element.boundingBox();
+    const x = box.x + box.width / 2;
+    const y = box.y + box.height / 2;
+
+    await page.mouse.move(x, y, { steps: randomDelay(20, 40) });
+    await page.mouse.click(x, y);
   };
 
   const clickAndWait = async (page, attemptCount = 1) => {
     try {
-      await page.evaluate((selector) => {
-        document.querySelector(selector).click();
-      }, selectors.serviceButton);
-  
+      await simulateUserInteraction(page, selectors.serviceButton);
+
       console.log(`Button clicked, waiting... (Attempt: ${attemptCount})`);
-      await sendMessage(`Tentativa: ${attemptCount}` )
+      await sendMessage(`Tentativa: ${attemptCount}`);
 
-      await new Promise(resolve => setTimeout(resolve, 120000));
-
+      await page.waitForNavigation({ waitUntil: 'networkidle0', timeout: 60000 });
       if (page.url() === urls.booking) {
         console.log('Booking page reached!');
         playSound();
-        sendMessage(msg.booking);
+        await sendMessage(msg.booking);
         // Pause execution without closing the browser
         await new Promise(() => {});
-      }
-  
-      await page.reload({ waitUntil: ['networkidle0', 'domcontentloaded'] });
-  
-      if (page.url() === urls.login) {
-        console.log('URL indicates logged out, logging in again');
-        await login(page);
-        await page.goto(urls.services);
-        await page.waitForSelector(selectors.serviceRow);
       } else {
-        await page.waitForSelector(selectors.serviceRow);
+        await page.reload({ waitUntil: ['networkidle0', 'domcontentloaded'] });
+
+        if (page.url() === urls.login) {
+          console.log('URL indicates logged out, logging in again');
+          await login(page);
+          await page.goto(urls.services);
+          await page.waitForSelector(selectors.serviceRow);
+        } else {
+          await page.waitForSelector(selectors.serviceRow);
+        }
+
+        await wait(60000); // Wait for 1 minute before the next attempt
+        await clickAndWait(page, attemptCount + 1);
       }
-  
-      await clickAndWait(page, attemptCount + 1);
     } catch (error) {
       console.error(`Error on attempt ${attemptCount}:`, error);
+      await wait(60000); // Wait for 1 minute before the next attempt
       await clickAndWait(page, attemptCount + 1);
     }
   };
-  
 
   try {
-
     console.log('Waiting for WhatsApp authentication...');
     await authenticate();
     console.log('WhatsApp authenticated.');
@@ -131,22 +158,23 @@ require('dotenv').config();
         '--ignore-certifcate-errors-spki-list',
         '--disable-features=IsolateOrigins,site-per-process',
         '--flag-switches-begin',
-        '--flag-switches-end'
+        '--flag-switches-end',
+        '--disable-blink-features=AutomationControlled'
       ]
     });
 
     const page = await browser.newPage();
     await configurePage(page);
 
-    await page.goto(urls.login, {timeout: 0});
+    await page.goto(urls.login, { timeout: 0 });
     await login(page);
-    await sendMessage(msg.login)
+    await sendMessage(msg.login);
 
     if (await page.$(selectors.serviceRow) === null) {
       await page.goto(urls.services);
       await page.waitForSelector(selectors.serviceRow);
     }
-    
+
     await clickAndWait(page);
 
     await browser.close();
